da95f372fb6f02dcecdc31db7c0fcd377566505f
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-20 16:14:37 -0600

    definiendo la clase MultiModSearch

---
+++ search.py
@@ -0,0 +1,32 @@
+import os 
+from haystack import Document
+from haystack import Pipeline
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.nodes.retriever.multimodal import MultiModalRetriever
+
+class MultiModSearch:
+    def __init__(self):
+        self.document_sotores = InMemoryDocumentStore(embedding_dim=512)
+        doc_dir = "img_data"
+        
+        images = [
+            Document(content= f"{doc_dir}/{filename}",content_type="image")
+            for filename in os.listdir(f"./{doc_dir}")
+        ]
+
+        self.document_sotores.write_documents(images)
+        self.retriever_text_to_image = MultiModalRetriever(
+            document_store= self.document_sotores,
+            query_embedding_model= "setence-transformers/clip-ViT-B-32",
+            query_type="text",
+            document_embedding_models= {"image":"setence-transformers/clip-ViT-B-32"}
+        )
+
+        self.document_sotores.update_embeddings(retriever= self.retriever_text_to_image)
+        
+        self.pipeline = Pipeline()
+        self.pipeline.add_node(component= self.retriever_text_to_image, name="retiever_text_to_image",inputs=["query"])
+
+    def search(self,query,top_k=3):
+        results = self.pipeline.run(query=query,params={"retriever_text_to_image":{"top_k":top_k}})
+        return sorted(results["documents"],key=lambda d:d.score,reverse=True)
